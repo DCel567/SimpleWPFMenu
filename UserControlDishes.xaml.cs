0d7126d711105a43d8c9d4b2cf5a620e9485f836
George <dcel567@gmail.com>
2021-05-01 17:43:32 +0300

    one and a half of project

---
+++ UserControlDishes.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantMenu {
+    /// <summary>
+    /// Interaction logic for UserControlDishes.xaml
+    /// </summary>
+    public partial class UserControlDishes : UserControl {
+        public UserControlDishes() {
+            InitializeComponent();
+        }
+    }
+}
